b8fe4d46d914adb56776d6cf160825dce432dffe
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-24 14:24:25 -0500

    fleshing out hero comp. update readme

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,7 +3,20 @@ import "./Hero.css";
 export function Hero() {
   return (
     <section className="hero-container">
-      <div>We deliver top level lawn care and landscaping</div>
+      <div className="hero-title">
+        <h1>
+          We deliver top level
+          <br />
+          lawn care and landscaping
+        </h1>
+        <p>
+          The art of lawn perfection, the science of beautiful landscapes, the
+          science of <br />
+          beautiful landscapes, designed to thrive
+        </p>
+      </div>
+
+      <img className="hero-img" src="./public/one.png" />
     </section>
   );
 }
