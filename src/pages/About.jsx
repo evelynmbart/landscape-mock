84509af0231c0313b4038ed0fc285c5d4f97becd
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-25 12:36:34 -0500

    finish about comp styling.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,19 +23,40 @@ export function About() {
         <img src="./public/five.png" />
       </div>
       <div className="about-right">
-        <h1></h1>
-        <p></p>
-        <p></p>
+        <h1>You're One Of Our Own At MowPro</h1>
+        <div className="about-summary">
+          <p>
+            MowPro is a locally owned landscaping company dedicated to
+            transforming yards with the utmost care and attention to detail.
+            With a personal touch and a commitment to our community, we ensure
+            every lawn feels like our own.
+          </p>
+          <p>
+            Our team of friendly professionals takes pride in providing
+            reliable, top-notch service that enhances the beauty of your outdoor
+            spaces, season after season.
+          </p>
+        </div>
+
         <div className="about-stats">
           {ABOUT_STATS.map((stat, index) => (
             <div key={index}>
-              <h2>{stat.amount}</h2>
-              <h5>{stat.description}</h5>
+              <h2 style={{ fontSize: "3rem" }}>{stat.amount}</h2>
+              <h5
+                style={{
+                  fontSize: "1rem",
+                  fontWeight: "normal",
+                  textTransform: "uppercase",
+                  letterSpacing: "1px",
+                }}
+              >
+                {stat.description}
+              </h5>
             </div>
           ))}
         </div>
+        <Button>Get Started</Button>
       </div>
-      <Button>Get Started</Button>
     </section>
   );
 }
